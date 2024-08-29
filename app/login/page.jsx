588c5741e1c440fa129f783542031381b5c9412d
Ayon <suprovatbit@gmail.com>
2024-08-29 11:28:50 +0600

    Login Page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,17 @@
+//Dependencies
+import styles from "@/app/ui/login/login.module.css";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1 className="font-extrabold text-3xl">Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
