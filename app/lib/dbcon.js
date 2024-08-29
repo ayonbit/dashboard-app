52591aa7c86e0e3e886215ef424631775d365da2
Ayon <suprovatbit@gmail.com>
2024-08-29 13:25:15 +0600

    mongo connections setup

---
+++ app/lib/dbcon.js
@@ -0,0 +1,17 @@
+//dependencies
+
+import mongoose from "mongoose";
+
+//db connection
+export const connectToDB = async () => {
+  const conncetion = {};
+  try {
+    if (conncetion.isConnected) return;
+    //db connection
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    conncetion.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
