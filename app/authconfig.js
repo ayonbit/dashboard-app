4f843fbe3fbd667705c6e2761a74045cf6131afb
Ayon <suprovatbit@gmail.com>
2024-09-04 16:15:13 +0600

    dashboard protected Next auth Config

---
+++ app/authconfig.js
@@ -0,0 +1,16 @@
+export const authConfig = {
+  providers: [],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request }) {
+      const isLoggedIn = auth?.user;
+      const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        return isLoggedIn ? true : false;
+      }
+      return true;
+    },
+  },
+};
